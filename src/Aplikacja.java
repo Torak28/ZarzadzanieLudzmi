7022fe588a1938f4bb79cb87e12c1a20c9197e18
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2016-12-31 15:09:43 +0100

    Poczatkowe klasy

---
+++ src/Aplikacja.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Torak28 on 31.12.2016.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aplikacja {
+	private List<Pracownik> WszyscyPracownicy = new ArrayList<Pracownik>();
+	private List<Zadanie> WszystkieZadania = new ArrayList<Zadanie>();
+
+	public getWszyscyPracownicy(){
+		return WszyscyPracownicy;
+	}
+	public setWszyscyPracownicy(){
+
+	}
+	public getWszystkieZadania(){
+		return WszystkieZadania;
+	}
+	public setWszystkieZadania(){
+
+	}
+}
---
+++ src/Zadanie.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Torak28 on 31.12.2016.
+ */
+public class Zadanie {
+	private int Typ;
+	private int Norma;
+	private Druzyna Wykonawcy;
+	private int Wynik;
+
+	private getTyp(){
+		return Typ;
+	}
+	private setTyp(){
+
+	}
+	private getNorma(){
+		return Norma;
+	}
+	private setNorma(){
+
+	}
+	private getDruzyna(){
+		return Druzyna;
+	}
+	private setDruzyna(){
+
+	}
+	private getWynik(){
+		return Wynik;
+	}
+	private setWynik() {
+
+	}
+}
